e06b4b16d8b6dccd8964251387f56be8fedd8b15
Gomhiram18 <54558498+Gomhiram18@users.noreply.github.com>
2019-11-06 18:19:49 -0500

    UpdateDoublepiHC

--- DoublePi.c
+++ DoublePi.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
+// Ejercicio resuelto aprenderaprogramar.com
 int main() {
-    double precio1, precio2, precio3, media;
-
-    printf ("Introduzca el precio en establecimiento 1, en euros: ");
-    scanf ("%lf", &precio1);
-    printf ("Introduzca el precio en establecimiento 2, en euros: ");
-    scanf ("%lf", &precio2);
-    printf ("Introduzca el precio en establecimiento 3, en euros: ");
-    scanf ("%lf", &precio3);
-
-    media = (precio1 + precio2 + precio3) / 3;
-    printf ("El precio medio del producto es de %lf euros", media);
-    return 0;
+    const double Pi = 3.141593; //Número pi como constante
+    double D, H, R, V;
+//  Datos
+    printf ("Introduzca el diametro, en metros: "); scanf ("%lf", &D);
+    printf ("Introduzca la altura, en metros: "); scanf ("%lf", &H);
+//Cálculo
+    R = D/2; V = Pi * pow(R, 2) * H;
+    printf ("El volumen del cilindro es de %lf metros cubicos", V);
+   return 0;
 }
